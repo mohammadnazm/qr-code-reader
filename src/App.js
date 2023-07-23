186872d5416d36c25352ca461ba2e298d994b18c
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-23 20:02:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,22 @@ function App() {
       <input type="text" onChange={e => setInputValue(e.target.value)}></input>
 
       <h3>2, QRCode Reader : qr-scanner package</h3>
+
+      <div
+        style={{
+          height: "auto",
+          margin: "0 auto",
+          maxWidth: 64,
+          width: "100%",
+        }}
+      >
+        <QRCode
+          size={256}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          value={inputValue}
+          viewBox={`0 0 256 256`}
+        />
+      </div>
     </div>
   )
 }
