a8bf9f8778493703e2dbdd430f0a9abefd8418c2
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-23 20:19:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ import QRCode from "react-qr-code"
 function App() {
   const [inputValue, setInputValue] = useState("")
 
+  // eslint-disable-next-line no-unused-vars
+  const download = () => {
+    const svg = document.getElementById("QRCode")
+    const svgData = new XMLSerializer().serializeToString(svg)
+    const canvas = document.createElement("canvas")
+    const ctx = canvas.getContext("2d")
+    const img = new Image()
+    img.onload = () => {
+      canvas.width = img.width
+      canvas.height = img.height
+      ctx.drawImage(img, 0, 0)
+      const pngFile = canvas.toDataURL("image/png")
+      const downloadLink = document.createElement("a")
+
+      // name of image
+      downloadLink.download = `${inputValue}`
+      downloadLink.href = `${pngFile}`
+      downloadLink.click()
+    }
+    img.src = `data:image/svg+xml;base64,${btoa(svgData)}`
+  }
+
   return (
     <div>
       <h3>1, QRCode Generator and Downloader : react-qr-code package</h3>
@@ -26,8 +48,12 @@ function App() {
           style={{ height: "auto", maxWidth: "100%", width: "100%" }}
           value={inputValue}
           viewBox={`0 0 256 256`}
+          id="QRCode"
         />
       </div>
+
+      <input type="text" onChange={e => setInputValue(e.target.value)}></input>
+      <input type="button" onClick={download} value="Download" />
     </div>
   )
 }
